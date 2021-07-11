a38572f8847e3cc621bb5ad2c6b0ce9892d798ac
12-153 <revathigade@gmail.com>
2021-07-11 13:03:10 -0500

    Testing update

--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,23 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsComponent } from './details.component';
+import {ProductService} from '../services/product.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
+  let productService:ProductService;
   let fixture: ComponentFixture<DetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
+      declarations: [ DetailsComponent ],
+      imports: [ NgbModule.forRoot() ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    productService = new ProductService(null);
+    component=new DetailsComponent(productService);
+     });
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,22 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {Router} from '@angular/router';
+import {ProductService} from '../services/product.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
+  let productService:ProductService;
+  let router: Router;
   let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ NgbModule.forRoot() ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    productService = new ProductService(null);
+    component= new HomeComponent(router,productService);
   });
 
   it('should create', () => {
